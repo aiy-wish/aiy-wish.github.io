542cd8b13b15acefe458c76e253e30faa91e068c
Ayush Chakraborty <ayushc@umd.edu>
2021-06-14 12:21:03 +0400

    Updates

---
+++ precache-manifest.a284a9d0192b1ac2fbac43a874b066d6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd9cfc418a795ccc86c4c921197a53bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "984a26c91c4fef982408",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984a26c91c4fef982408",
+    "url": "/static/js/main.a1c65cf2.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
